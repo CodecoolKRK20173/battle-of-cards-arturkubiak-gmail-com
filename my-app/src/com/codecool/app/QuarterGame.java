357296b85b0b6394cf24a136904ce7efd8e2fee4
Artur <arturkubiak9@gmail.com>
2020-06-25 19:43:15 +0200

    Add split deck of cards on two players

--- my-app/src/com/codecool/app/QuarterGame.java
+++ my-app/src/com/codecool/app/QuarterGame.java
@@ -1,5 +1,6 @@
 package com.codecool.app;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuarterGame {
@@ -12,10 +13,24 @@ public class QuarterGame {
     }
 
     void run() {
-        System.out.println(this.deckToGame.toString());
-        Card card = this.deckToGame.get(0);
-        this.deckToGame.remove(this.deckToGame.get(0));
-        System.out.println(deckToGame.toString());
-        System.out.println(card.toString());
+        List<Card> deckFirstPlayer = new ArrayList<>();
+        List<Card> deckSecondPlayer = new ArrayList<>();
+        int sizeOfDeck = deckToGame.size();
+
+        for (int index = 0; index < sizeOfDeck; index++) {
+            Card card = this.deckToGame.get(0);
+            this.deckToGame.remove(this.deckToGame.get(0));
+
+            if (index % 2 == 0) {
+                deckFirstPlayer.add(card);
+            }
+            else {
+                deckSecondPlayer.add(card);
+            }
+        }
+        System.out.println(deckFirstPlayer.toString());
+        System.out.println(deckFirstPlayer.size());
+        System.out.println(deckSecondPlayer.toString());
+        System.out.println(deckSecondPlayer.size());
     }
 }
